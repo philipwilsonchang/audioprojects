62be4f271fa47736fd1ced479af064c6c33f9466
philip.wilson.chang@gmail.com <philip.wilson.chang@gmail.com>
2018-08-29 16:43:28 -0500

    Resolved TFRecord reading issue - float32 not float64

--- tfRecordReading.py
+++ tfRecordReading.py
@@ -14,36 +14,30 @@ from scipy import signal
 # for example in tf.python_io.tf_record_iterator("../nsynth-test.tfrecord"):
 # 	print(tf.train.Example.FromString(example))
 
-
-# Transforms tf.Dataset Example proto into an array of features containing data
-# Input: TFRecord Example proto
-# Output: Array of Tensors - one for each feature
-def parse_tfrecord(tfrecord_example):
-	feature_list = {'note': tf.FixedLenFeature(shape=[], dtype=tf.int64),
+feature_list = {'note': tf.FixedLenFeature(shape=[], dtype=tf.int64),
 				'note_str': tf.FixedLenFeature(shape=[], dtype=tf.string),
 				'instrument': tf.FixedLenFeature(shape=[], dtype=tf.int64),
 				'instrument_str': tf.FixedLenFeature(shape=[], dtype=tf.string),
 				'pitch': tf.FixedLenFeature(shape=[], dtype=tf.int64),
 				'velocity': tf.FixedLenFeature(shape=[], dtype=tf.int64),
 				'sample_rate': tf.FixedLenFeature(shape=[], dtype=tf.int64),
-				'audio': tf.FixedLenFeature(shape=[1], dtype=tf.float64),
+				'audio': tf.FixedLenFeature(shape=[1], dtype=tf.float32),
 				'qualities': tf.FixedLenFeature(shape=[1], dtype=tf.int64),
 				'qualities_str': tf.FixedLenFeature(shape=[1], dtype=tf.string),
 				'instrument_family': tf.FixedLenFeature(shape=[], dtype=tf.int64),
 				'instrument_family_str': tf.FixedLenFeature(shape=[], dtype=tf.string),
 				'instrument_source': tf.FixedLenFeature(shape=[], dtype=tf.int64),
 				'instrument_source_str': tf.FixedLenFeature(shape=[], dtype=tf.string)}
-	parsed_features = tf.parse_single_example(tfrecord_example, feature_list)
-	return parsed_features
+
 
 # Import training data, read all examples, extract features
-# tfrecordname = ["../data/nsynth-test.tfrecord"]
+tfrecordname = ["../data/nsynth-test.tfrecord"]
 # train_dataset = tf.data.TFRecordDataset(tfrecordname) # Parse entire dataset
 # train_dataset = train_dataset.map(parse_tfrecord)
 
-# Import single data example features
-tfrecordname = "../data/nsynth-test.tfrecord"
-tf_example = tf.python_io.tf_record_iterator(tfrecordname).next()
-example_data = parse_tfrecord(tf_example)
+filename_queue = tf.train.string_input_producer(["../data/nsynth-test.tfrecord"])
+reader = tf.TFRecordReader()
+_, serialized = reader.read(filename_queue)
+features = tf.parse_single_example(serialized, feature_list)
 
-print(example_data)
+print(features)
